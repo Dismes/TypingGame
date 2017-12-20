91e3de9620d527146fd297ab7a2d064abdcb0d49
Dismes <yuhki.n@gmail.com>
2017-12-19 20:19:48 -0800

    test

--- ClientSide/assets/js/main.js
+++ ClientSide/assets/js/main.js
@@ -4,25 +4,30 @@ const seconds = 10;
 var button = $("#startButton");
 var scoreDiv = $("#score");
 var words = $("ExampleInput");
+var typed;
 
 
 $(document).ready(function () {
     console.log("this is a test ");
 
-    button.click(function () {
+    button.click(function (e) {
         countdown();
-    });
 
+    });
+    
+    document.addEventListener("keydown", typing, false);
+    
+    
 });
 
 
 function countdown() {
+    console.log("this is should be the last button pressed " + typed)
     time = seconds;
     timerDiv.text(time);
     points = 0;
     button.prop("disabled", true);
     var timer = setInterval(function () {
-        
         time--;
         timerDiv.text(time);
         if (time === 0) {
@@ -36,3 +41,15 @@ function countdown() {
         }
     }, 1000);
 }
+
+
+
+function typing(e){
+    console.log(e.which);
+    typed = String.fromCharCode(e.which);
+    console.log(typed);
+    return typed;
+}
+
+
+
